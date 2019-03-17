20f62c59542509d12b7b668e5350bc9211eeb9f3
YalinA <YalinA@YalinA-Laptop.dm.cx>
2019-03-17 22:47:32 +0200

    Update 17/03/19 22:00

--- Coupon-Code/src/Java/Main/CouponSystem.java
+++ Coupon-Code/src/Java/Main/CouponSystem.java
@@ -16,7 +16,7 @@ public class CouponSystem {
 	/************************************** GET-DAO ******************************/
 
 	public enum clientType {
-		Admin, Customer, Company
+		Admin, Customer, Company, CustomerGold, CustomerSilver
 	};
 
 	private CouponSystem() {
@@ -30,7 +30,7 @@ public class CouponSystem {
 	public CouponClientFacade login(String name, String password, clientType cType) throws Exception {
 		// TODO Auto-generated method stub
 
-		switch (cType) {
+		switch (cType) { // according to cType it will execute the case
 		case Admin:
 			AdminFacade adminFacade = new AdminFacade();
 			if (adminFacade.login(name, password, cType)) {
@@ -67,9 +67,33 @@ public class CouponSystem {
 				return null;
 			}
 
+		case CustomerGold:
+			CompanyFacade companyFacade1 = new CompanyFacade();
+			if (companyFacade1.login(name, password, cType)) {
+				return companyFacade1;
+
+			} else {
+				JFrame frame = new JFrame("JOptionPane showMessageDialog example");
+				JOptionPane.showMessageDialog(frame,
+						"The login failed, the Name or Password of the Company is not valid, please try again !!!!");
+				return null;
+
+			}
+
+		case CustomerSilver:
+			CompanyFacade companyFacade2 = new CompanyFacade();
+			if (companyFacade2.login(name, password, cType)) {
+				return companyFacade2;
+
+			} else {
+				JFrame frame = new JFrame("JOptionPane showMessageDialog example");
+				JOptionPane.showMessageDialog(frame,
+						"The login failed, the Name or Password of the Company is not valid, please try again !!!!");
+				return null;
+
+			}
 		}
 		return null;
-
 	}
 
 	public void ShutDown() throws Exception {
