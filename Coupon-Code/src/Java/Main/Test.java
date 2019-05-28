bf88b0e8528085dd197cb4cd5354b20c94fc0370
YalinA <YalinA@YalinA-Laptop.dm.cx>
2019-05-28 09:21:54 +0300

    Update 28/05/2019

--- Coupon-Code/src/Java/Main/Test.java
+++ Coupon-Code/src/Java/Main/Test.java
@@ -1,10 +1,10 @@
 package Java.Main;
 
 import DB.Database;
+import Facade.AdminFacade;
 import Java.JavaBean.Company;
-import Java.JavaBean.Coupon;
-import Java.JavaBean.CouponType;
 import Java.JavaBean.Customer;
+import Java.Main.CouponSystem.clientType;
 
 public class Test {
 
@@ -27,9 +27,9 @@ public class Test {
 		// Utils.endDate(20), 7000, CouponType.CAMPING, "wtf",
 		// 251, "image");
 
-		Customer customer1 = new Customer(1, "Evi", "345345");
-		Customer customer2 = new Customer(2, "Shay", "567567");
-		Customer customer3 = new Customer(3, "Moahe", "345345");
+		// Customer customer1 = new Customer(1, "Evi", "345345");
+		// Customer customer2 = new Customer(2, "Shay", "567567");
+		// Customer customer3 = new Customer(3, "Moahe", "345345");
 
 		// Customer customer4 = new Customer(4, "Eli", "334455");
 		// Customer customer5 = new Customer(5, "Yossi", "889977");
@@ -39,12 +39,15 @@ public class Test {
 		// Customer customer9 = new Customer(9, "Sarge", "546546");
 		// Customer customer10 = new Customer(10, "Jurgen", "868686");
 
-		Coupon coupon1 = new Coupon(1, "Omer", Utils.getDate(), Utils.endDate(60), 1111, CouponType.HEALTH, "wtf", 1251,
-				"image");
-		Coupon coupon2 = new Coupon(2, "Oren", Utils.getDate(), Utils.endDate(60), 2222, CouponType.HEALTH, "abc", 3356,
-				"image");
-		Coupon coupon3 = new Coupon(3, "Ramon", Utils.getDate(), Utils.endDate(60), 3333, CouponType.HEALTH, "der",
-				4455, "image");
+		// Coupon coupon1 = new Coupon(1, "Omer", Utils.getDate(), Utils.endDate(60),
+		// 1111, CouponType.HEALTH, "wtf", 1251,
+		// "image");
+		// Coupon coupon2 = new Coupon(2, "Oren", Utils.getDate(), Utils.endDate(60),
+		// 2222, CouponType.HEALTH, "abc", 3356,
+		// "image");
+		// Coupon coupon3 = new Coupon(3, "Ramon", Utils.getDate(), Utils.endDate(60),
+		// 3333, CouponType.HEALTH, "der",
+		// 4455, "image");
 
 		// Coupon coupon4 = new Coupon(4, "Sarge", Utils.getDate(), Utils.endDate(60),
 		// 4444, CouponType.HEALTH, "nmmm",
@@ -70,134 +73,138 @@ public class Test {
 		// 2222, CouponType.HEALTH, "abc", 3356,
 		// "image");
 
-		// Customer customer1 = new Customer(1, "Evi", "898989");
-		// Customer customer2 = new Customer(2, "Shay", "567567");
+		Customer customer1 = new Customer(1, "Evi", "898989");
+		Customer customer2 = new Customer(2, "Shay", "567567");
 
 		// AdminFacade adminFacade = new AdminFacade();
 
 		// CustomerDBDAO customerDBDAO = new CustomerDBDAO();
 
 		/********* Admin **************/
-		// AdminFacade adminFacade2 = new AdminFacade();
-		// adminFacade2 = (AdminFacade) CouponSystem.getCouponSystem().login("admin",
-		// "12346", clientType.Admin);
-		// if (adminFacade2 != null) {
-		// adminFacade2.createCompany(company3);
-		// adminFacade2.getAllCompanies();
-		// adminFacade2.getAllCustomers();
+		AdminFacade adminFacade2 = new AdminFacade();
+		adminFacade2 = (AdminFacade) CouponSystem.getCouponSystem().login("admin", "12346", clientType.Admin);
+		if (adminFacade2 != null) {
+			// adminFacade2.createCompany(company1);
+			// adminFacade2.createCompany(company2);
+			// adminFacade2.createCompany(company3);
+			// adminFacade2.createCustomer(customer1);
+			// adminFacade2.createCustomer(customer1);
+			// adminFacade2.getAllCompanies();
+			// adminFacade2.getAllCustomers();
 
-		/********* Company **************/
-		// CompanyFacade companyfacade = new CompanyFacade();
-		// companyfacade.login("mPresnt", "12345", clientType.Company);
-		// companyfacade = (CompanyFacade)
-		// CouponSystem.getCouponSystem().login("mPrest", "12345", clientType.Company);
-		// if (companyfacade != null) {
-		// companyfacade.createCoupon(coupon1);
-		// companyfacade.getCouponsByExpiredDate(Utils.getDate());
+			/********* Company **************/
+			// CompanyFacade companyfacade = new CompanyFacade();
+			// companyfacade.login("mPresnt", "12345", clientType.Company);
+			// companyfacade = (CompanyFacade)
+			// CouponSystem.getCouponSystem().login("mPrest", "12345", clientType.Company);
+			// if (companyfacade != null) {
+			// companyfacade.createCoupon(coupon1);
+			// companyfacade.getCouponsByExpiredDate(Utils.getDate());
 
-		// }
+			// }
 
-		/********* Customer **************/
+			/********* Customer **************/
 
-		// CustomerFacade CustomerFacade = new CustomerFacade();
-		// CustomerFacade = (CustomerFacade) CouponSystem.getCouponSystem().login("Evi",
-		// "12345", clientType.Customer);
-		// if (companyfacade != null) {
-		// CustomerFacade.purchaseCoupon(coupon1);
-		// }
+			// CustomerFacade CustomerFacade = new CustomerFacade();
+			// CustomerFacade = (CustomerFacade) CouponSystem.getCouponSystem().login("Evi",
+			// "12345", clientType.Customer);
+			// if (companyfacade != null) {
+			// CustomerFacade.purchaseCoupon(coupon1);
+			// }
 
-		// adminFacade2.getCompanybyPW("55662322");
+			// adminFacade2.getCompanybyPW("55662322");
 
-		// }
+			// }
 
-		// customerDBDAO.purchaseCoupon(coupon1, customer1);
-		// customerDBDAO.getCustomer1("898989");
+			// customerDBDAO.purchaseCoupon(coupon1, customer1);
+			// customerDBDAO.getCustomer1("898989");
 
-		// customerDBDAO.createCustomer(customer2);
-		// customerDBDAO.getCustomer("Evi");
-		// customerDBDAO.getCoupons();
-		// customerDBDAO.getAllCustomer();
-		// customerDBDAO.getCustomerCoupons(customer1);
-		// customerDBDAO.printAllCustmers();
+			// customerDBDAO.createCustomer(customer2);
+			// customerDBDAO.getCustomer("Evi");
+			// customerDBDAO.getCoupons();
+			// customerDBDAO.getAllCustomer();
+			// customerDBDAO.getCustomerCoupons(customer1);
+			// customerDBDAO.printAllCustmers();
 
-		// customerDBDAO.updateCustomer(customer1);
-		// customerDBDAO.
+			// customerDBDAO.updateCustomer(customer1);
+			// customerDBDAO.
 
-		// adminFacade.createCompany(company1);
-		// adminFacade.createCompany(company2);
+			// adminFacade.createCompany(company1);
+			// adminFacade.createCompany(company2);
 
-		// adminFacade.removeCompany(company2);
-		// adminFacade.removeCompany(company1);
-		// adminFacade.getAllCompanies();
-		// adminFacade.updateCustomer(customer1);
-		// adminFacade.createCustomer(customer2);
+			// adminFacade.removeCompany(company2);
+			// adminFacade.removeCompany(company1);
+			// adminFacade.getAllCompanies();
+			// adminFacade.updateCustomer(customer1);
+			// adminFacade.createCustomer(customer2);
 
-		// adminFacade.updateCustomer(customer1);
+			// adminFacade.updateCustomer(customer1);
 
-		// CompanyFacade companyFacade = new CompanyFacade();
-		// CouponDBDAO couponDBDAO = new CouponDBDAO();
+			// CompanyFacade companyFacade = new CompanyFacade();
+			// CouponDBDAO couponDBDAO = new CouponDBDAO();
 
-		// couponDBDAO.createCoupon(coupon1, 1);
-		// couponDBDAO.createCoupon(coupon2, 2);
+			// couponDBDAO.createCoupon(coupon1, 1);
+			// couponDBDAO.createCoupon(coupon2, 2);
 
-		// adminFacade.removeCompany(company2);
-		// couponDBDAO.removeCoupon(coupon1);
-		// couponDBDAO.removeCustomerCoupon(coupon2);
+			// adminFacade.removeCompany(company2);
+			// couponDBDAO.removeCoupon(coupon1);
+			// couponDBDAO.removeCustomerCoupon(coupon2);
 
-		// couponDBDAO.createCoupon(coupon1, 1);
-		// couponDBDAO.createCoupon(coupon2, 2);
+			// couponDBDAO.createCoupon(coupon1, 1);
+			// couponDBDAO.createCoupon(coupon2, 2);
 
-		// companyFacade.createCoupon(coupon1);
-		// companyFacade.createCoupon(coupon2);
+			// companyFacade.createCoupon(coupon1);
+			// companyFacade.createCoupon(coupon2);
 
-		// adminFacade.createCompany(company1);
-		// adminFacade.createCompany(company2);
+			// adminFacade.createCompany(company1);
+			// adminFacade.createCompany(company2);
 
-		// Company company3 = new Company(3, "Checkmarx", "121212", "Uria@test.com");
-		// Company company4 = new Company(4, "Greenlight", "445566", "Leon@test.com");
-		// Company company5 = new Company(5, "Kobicom", "889944", "Meital@test.com");
-		// Company company6 = new Company(6, "Amdocs", "525252", "Jurgen@test.com");
-		// Company company7 = new Company(7, "Giga", "665588", "Serge@test.com");
-		// Company company8 = new Company(8, "Huhuea", "889955", "Emual@test.com");
-		// Company company9 = new Company(9, "Moshecom", "585858", "David@test.com");
-		// Company company10 = new Company(10, "Yyuvalcom", "4454545",
-		// "Ramon@test.com");
+			// Company company3 = new Company(3, "Checkmarx", "121212", "Uria@test.com");
+			// Company company4 = new Company(4, "Greenlight", "445566", "Leon@test.com");
+			// Company company5 = new Company(5, "Kobicom", "889944", "Meital@test.com");
+			// Company company6 = new Company(6, "Amdocs", "525252", "Jurgen@test.com");
+			// Company company7 = new Company(7, "Giga", "665588", "Serge@test.com");
+			// Company company8 = new Company(8, "Huhuea", "889955", "Emual@test.com");
+			// Company company9 = new Company(9, "Moshecom", "585858", "David@test.com");
+			// Company company10 = new Company(10, "Yyuvalcom", "4454545",
+			// "Ramon@test.com");
 
-		// Company company11 = new Company(11, "Nanalcom", "55669988", "Oren@test.com");
-		// Company company12 = new Company(12, "Walla", "3344556677",
-		// "Ruobem@test.com");
+			// Company company11 = new Company(11, "Nanalcom", "55669988", "Oren@test.com");
+			// Company company12 = new Company(12, "Walla", "3344556677",
+			// "Ruobem@test.com");
 
-		// CompanyDBDAO companyDBDAO = new CompanyDBDAO();
+			// CompanyDBDAO companyDBDAO = new CompanyDBDAO();
 
-		// companyDBDAO.createCompany(company11);
+			// companyDBDAO.createCompany(company11);
 
-		// CustomerDBDAO customerDBDAO = new CustomerDBDAO();
-		// customerDBDAO.createCustomer(customer1);
-		// customerDBDAO.createCustomer(customer2);
-		// customerDBDAO.createCustomer(customer3);
-		// customerDBDAO.createCustomer(customer4);
-		// customerDBDAO.createCustomer(customer5);
-		// customerDBDAO.createCustomer(customer6);
-		// customerDBDAO.createCustomer(customer7);
-		// customerDBDAO.createCustomer(customer8);
-		// customerDBDAO.createCustomer(customer9);
-		// customerDBDAO.createCustomer(customer10);
+			// CustomerDBDAO customerDBDAO = new CustomerDBDAO();
+			// customerDBDAO.createCustomer(customer1);
+			// customerDBDAO.createCustomer(customer2);
+			// customerDBDAO.createCustomer(customer3);
+			// customerDBDAO.createCustomer(customer4);
+			// customerDBDAO.createCustomer(customer5);
+			// customerDBDAO.createCustomer(customer6);
+			// customerDBDAO.createCustomer(customer7);
+			// customerDBDAO.createCustomer(customer8);
+			// customerDBDAO.createCustomer(customer9);
+			// customerDBDAO.createCustomer(customer10);
 
-		// CouponDBDAO couponDBDAO = new CouponDBDAO();
-		// couponDBDAO.createCoupon(coupon8, 1);
+			// CouponDBDAO couponDBDAO = new CouponDBDAO();
+			// couponDBDAO.createCoupon(coupon8, 1);
 
-		// couponDBDAO.createCoupon(coupon1);
-		// couponDBDAO.createCoupon(coupon2);
-		// couponDBDAO.createCoupon(coupon3);
-		// couponDBDAO.createCoupon(coupon4);
-		// couponDBDAO.createCoupon(coupon5);
+			// couponDBDAO.createCoupon(coupon1);
+			// couponDBDAO.createCoupon(coupon2);
+			// couponDBDAO.createCoupon(coupon3);
+			// couponDBDAO.createCoupon(coupon4);
+			// couponDBDAO.createCoupon(coupon5);
 
-		// customerDBDAO.getCustomer(1);
-		// customerDBDAO.printAllCustmers();
+			// customerDBDAO.getCustomer(1);
+			// customerDBDAO.printAllCustmers();
 
-		// customerDBDAO.createCustomer(customer1);
-		// customerDBDAO.removeCustomer(customer1);
-		// customerDBDAO.removeCustomer(customer2);
-		// customerDBDAO.removeCustomer(customer3);
+			// customerDBDAO.createCustomer(customer1);
+			// customerDBDAO.removeCustomer(customer1);
+			// customerDBDAO.removeCustomer(customer2);
+			// customerDBDAO.removeCustomer(customer3);
+		}
 	}
 }
